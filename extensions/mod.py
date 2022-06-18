8a45b3ba0aaea68d8cedecf09f20cf6cc207a3a4
carsonful <carsonhcf@gmail.com>
2022-06-18 15:34:13 -0400

    Added Moderator Command

---
+++ extensions/mod.py
@@ -0,0 +1,58 @@
+import asyncio
+
+import hikari
+import lightbulb
+from lightbulb import errors
+
+mod_plugin = lightbulb.Plugin("Mod")
+
+
+@mod_plugin.command
+@lightbulb.add_checks(
+    lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
+    lightbulb.bot_has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES)
+)
+@lightbulb.option(
+    "messages", "The number of messages to purge.", type=int, required=True
+)
+@lightbulb.command("purge", "Purge messages.", aliases=["clear"])
+@lightbulb.implements(lightbulb.SlashCommand)
+async def purge_messages(ctx: lightbulb.Context) -> None:
+    num_msgs = ctx.options.messages
+    channel = ctx.channel_id
+
+    # If the command was invoked using the PrefixCommand, it will create a message
+    # before we purge the messages, so you want to delete this message first
+    if isinstance(ctx, lightbulb.PrefixContext):
+        await ctx.event.message.delete()
+
+    msgs = await ctx.bot.rest.fetch_messages(channel).limit(num_msgs)
+    await ctx.bot.rest.delete_messages(channel, msgs)
+
+    resp = await ctx.respond(f"{len(msgs)} messages deleted")
+
+    await asyncio.sleep(5)
+    await resp.delete()
+
+@mod_plugin.command
+@lightbulb.add_checks(
+    lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
+    lightbulb.bot_has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES)
+)
+@lightbulb.option(
+    "number", "The number of messages to spam.", type=int, required=True
+)
+@lightbulb.command('spam','SPAM THE MF CHAT')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def spam_msg(ctx: lightbulb.Context) -> None:
+    f = hikari.File('C:\\Users\\carso\\Desktop\\CARS-DISC-main\\awda.png')
+    for i in range(ctx.options.number):
+        await ctx.respond('L CORD + L LIFE + L OWNER')
+
+
+
+
+
+
+def load(bot: lightbulb.BotApp) -> None:
+    bot.add_plugin(mod_plugin)
